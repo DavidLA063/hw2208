7a4074dc7cf16908c3eee68635f098d85a13d1d0
David <david063@mail.ru>
2022-08-23 22:43:29 +0300

    add ex10 print three number

---
+++ ex13_threeNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿int [] array = {1, 2, 3, 4, 5, 6, 7};
+
+Console.WriteLine ("Введите число ");
+
+int day = int.Parse(Console.ReadLine());
+
+if (day==array[0] || day==array[1] || day==array[2] || day==array[3] || day==array[4])
+    Console.WriteLine("нет");
+    else {
+    Console.WriteLine("да");
+}
