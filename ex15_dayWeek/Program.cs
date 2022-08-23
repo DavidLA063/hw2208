a0e1d67972e1a1e0c3d3c6202f614fdc27838c20
David <david063@mail.ru>
2022-08-23 22:45:07 +0300

    add ex10 print day Week

---
+++ ex15_dayWeek/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine ("Введите любое число: ");
+
+int a = int.Parse (Console.ReadLine ());
+int result = a % 10;
+
+if (a < 100) {
+    Console.WriteLine ("Третьей цифры нет");  
+}
+    else {
+        while (a > 1000)
+        {
+            a = a / 10;
+            result = a % 10;
+        }
+        Console.WriteLine ($"Третья цифра вашего числа {result}");
+    }
