faf1ce9918a06a4ec7476d174bf5feb82c4ecf0c
David <david063@mail.ru>
2022-08-23 22:41:02 +0300

    add ex10 print second number

---
+++ ex10_secondNumber/Program.cs
@@ -0,0 +1,9 @@
+﻿int Second (int a) {
+    return (a % 100) / 10 ;
+}
+
+Console.WriteLine ("Введите треххначное число: ");
+
+int numberUser = int.Parse(Console.ReadLine());
+
+Console.WriteLine (Second (numberUser));
