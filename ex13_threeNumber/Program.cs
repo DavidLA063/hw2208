ac1a45f49614531007e74686a15e6ee07cfcd91a
David <david063@mail.ru>
2022-08-24 12:10:47 +0300

    add function

--- ex13_threeNumber/Program.cs
+++ ex13_threeNumber/Program.cs
@@ -1,4 +1,5 @@
-﻿int [] array = {1, 2, 3, 4, 5, 6, 7};
+﻿/*
+int [] array = {1, 2, 3, 4, 5, 6, 7};
 
 Console.WriteLine ("Введите число ");
 
@@ -9,3 +10,19 @@ if (day==array[0] || day==array[1] || day==array[2] || day==array[3] || day==arr
     else {
     Console.WriteLine("да");
 }
+*/
+
+bool DayWeek(int arg1) {
+    if (arg1 == 6 || arg1 == 7)  {
+        return true;
+        }
+        else {
+            return false;
+        }
+}
+
+Console.WriteLine ("Введите число ");
+
+int day = int.Parse(Console.ReadLine());
+
+Console.WriteLine (DayWeek(day));
