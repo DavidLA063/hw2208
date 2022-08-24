43228e25ad7af81d33320d48d8e62f8965be014b
David <david063@mail.ru>
2022-08-24 12:19:52 +0300

    add function

--- ex13_threeNumber/Program.cs
+++ ex13_threeNumber/Program.cs
@@ -1,28 +1,16 @@
-﻿/*
-int [] array = {1, 2, 3, 4, 5, 6, 7};
+﻿Console.WriteLine ("Введите любое число: ");
 
-Console.WriteLine ("Введите число ");
+int a = int.Parse (Console.ReadLine ());
+int result = a % 10;
 
-int day = int.Parse(Console.ReadLine());
-
-if (day==array[0] || day==array[1] || day==array[2] || day==array[3] || day==array[4])
-    Console.WriteLine("нет");
+if (a < 100) {
+    Console.WriteLine ("Третьей цифры нет");  
+}
     else {
-    Console.WriteLine("да");
-}
-*/
-
-bool DayWeek(int arg1) {
-    if (arg1 == 6 || arg1 == 7)  {
-        return true;
+        while (a > 1000)
+        {
+            a = a / 10;
+            result = a % 10;
         }
-        else {
-            return false;
-        }
-}
-
-Console.WriteLine ("Введите число ");
-
-int day = int.Parse(Console.ReadLine());
-
-Console.WriteLine (DayWeek(day));
+        Console.WriteLine ($"Третья цифра вашего числа {result}");
+    }
--- ex15_dayWeek/Program.cs
+++ ex15_dayWeek/Program.cs
@@ -1,16 +1,29 @@
-﻿Console.WriteLine ("Введите любое число: ");
+﻿
+    /*
+int [] array = {1, 2, 3, 4, 5, 6, 7};
 
-int a = int.Parse (Console.ReadLine ());
-int result = a % 10;
+Console.WriteLine ("Введите число ");
 
-if (a < 100) {
-    Console.WriteLine ("Третьей цифры нет");  
-}
+int day = int.Parse(Console.ReadLine());
+
+if (day==array[0] || day==array[1] || day==array[2] || day==array[3] || day==array[4])
+    Console.WriteLine("нет");
     else {
-        while (a > 1000)
-        {
-            a = a / 10;
-            result = a % 10;
+    Console.WriteLine("да");
+}
+*/
+
+bool DayWeek(int arg1) {
+    if (arg1 == 6 || arg1 == 7)  {
+        return true;
         }
-        Console.WriteLine ($"Третья цифра вашего числа {result}");
-    }
+        else {
+            return false;
+        }
+}
+
+Console.WriteLine ("Введите число ");
+
+int day = int.Parse(Console.ReadLine());
+
+Console.WriteLine (DayWeek(day));
